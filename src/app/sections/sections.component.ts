e3d268a8e2b80142f482d45107ca366293d4cc11
Mirko <microvvavve@gmail.com>
2020-12-28 21:34:02 +0200

    Amounts to sections and cleaning

--- src/app/sections/sections.component.ts
+++ src/app/sections/sections.component.ts
@@ -8,22 +8,22 @@ import {BaseComponent} from '@shared/components/base/base.component';
 })
 export class SectionsComponent extends BaseComponent implements OnInit {
 	categories = [
-		'blog',
-		'call-to-action',
-		'contact',
-		'faq',
-		'feature',
-		'footer',
-		'form',
-		'header',
-		'hero',
-		'login',
-		'pricing',
-		'stats',
-		'steps',
-		'team',
-		'testimonial',
-		'timeline',
+		{name: 'blog', count: 2},
+		{name: 'call-to-action', count: 6},
+		{name: 'contact', count: 3},
+		{name: 'faq', count: 1},
+		{name: 'feature', count: 5},
+		{name: 'footer', count: 2},
+		{name: 'form', count: 1},
+		{name: 'header', count: 2},
+		{name: 'hero', count: 2},
+		{name: 'login', count: 2},
+		{name: 'pricing', count: 3},
+		{name: 'stats', count: 4},
+		{name: 'steps', count: 2},
+		{name: 'team', count: 3},
+		{name: 'testimonial', count: 3},
+		{name: 'timeline', count: 3},
 	];
 
 	constructor() {
