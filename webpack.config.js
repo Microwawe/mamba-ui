e8f868e5be6141685243da182e56161144e75f7d
Mirko Nowak <26537592+Microwawe@users.noreply.github.com>
2021-09-09 21:40:23 +0300

    Enable JIT-mode and update dependencies (#41)
    * chore: enable tailwind jit
    
    *chore: clean up color classes to reduce bundle size
    
    * chore: update angular 10 -> 12
    
    * fix: add bg color to all images
    
    * chore: update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,5 @@
 /* eslint-disable node/no-unsupported-features/es-syntax */
 /* eslint-disable node/no-unpublished-require */
-const purgecss = require('@fullhuman/postcss-purgecss')({
-	content: ['./src/**/*.html', './src/**/*.ts'],
-	safelist: {
-		standard: [
-			/(bg|text|border|ring)-(coolGray|yellow|amber|orange|red|rose|pink|fuchsia|purple|violet|indigo|blue|sky|lightBlue|cyan|teal|emerald|green|lime)-[1-9][0]?0$/,
-			/(from|to)-coolGray-[1-9]00$/,
-			/ring-offset-(coolGray)-[18]00$/,
-		],
-		greedy: [/token$/],
-	},
-	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-});
 
 module.exports = config => {
 	console.log(`Using '${config.mode}' mode`);
@@ -27,8 +15,7 @@ module.exports = config => {
 						plugins: [
 							require('postcss-import'),
 							require('tailwindcss'),
-							require('autoprefixer'),
-							...(config.mode === 'production' ? [purgecss] : []),
+							...(config.mode === 'production' ? [require('autoprefixer')] : []),
 						],
 					},
 				},
