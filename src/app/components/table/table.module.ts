168fce7ea99ef5a52c32efdeaa5cc0e3eddb807d
Mirko Nowak <26537592+Microwawe@users.noreply.github.com>
2022-01-29 16:39:44 +0200

    Dependencies + new tables (#51)
    * Updates part 1
    * Update Angular 12 -> 13
    * Update typescript, angular cache to gitignore
    * feat: Tables

--- src/app/components/table/table.module.ts
+++ src/app/components/table/table.module.ts
@@ -5,6 +5,8 @@ import {SharedModule} from '@shared/shared.module';
 import {TableShowcaseComponent} from './table-showcase.component';
 import {Table1Component} from './table1/table1.component';
 import {Table2Component} from './table2/table2.component';
+import {Table3Component} from './table3/table3.component';
+import {Table4Component} from './table4/table4.component';
 
 const routes: Routes = [
 	{
@@ -15,6 +17,12 @@ const routes: Routes = [
 
 @NgModule({
 	imports: [RouterModule.forChild(routes), SharedModule],
-	declarations: [TableShowcaseComponent, Table1Component, Table2Component],
+	declarations: [
+		TableShowcaseComponent,
+		Table1Component,
+		Table2Component,
+		Table3Component,
+		Table4Component,
+	],
 })
 export class TableModule {}
