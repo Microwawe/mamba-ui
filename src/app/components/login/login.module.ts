3bb56bc07d4ff24f9c91381835058447d0c9fa88
Mirko <microvvavve@gmail.com>
2022-01-30 10:59:40 +0200

    feat: login

--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -7,6 +7,7 @@ import {Login1Component} from './login1/login1.component';
 import {Login2Component} from './login2/login2.component';
 import {Login3Component} from './login3/login3.component';
 import {Login4Component} from './login4/login4.component';
+import {Login5Component} from './login5/login5.component';
 
 const routes: Routes = [
 	{
@@ -23,6 +24,7 @@ const routes: Routes = [
 		Login2Component,
 		Login3Component,
 		Login4Component,
+		Login5Component,
 	],
 })
 export class LoginModule {}
