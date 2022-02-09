fc1ab67d9f8379ab01662bdb4582ee04f22e9af1
Mirko Nowak <mirko.nowak@outlook.com>
2022-02-09 14:43:28 +0200

    feat: select to copy light or dark theme

--- src/app/shared/components/show-code/show-code.component.ts
+++ src/app/shared/components/show-code/show-code.component.ts
@@ -26,6 +26,7 @@ export class ShowCodeComponent extends BaseComponent implements AfterViewInit, O
 	code = '';
 	copied = false;
 	codeVisible = false;
+	dropdownOpen = false;
 	options = {
 		language: '',
 		component: '',
@@ -55,14 +56,25 @@ export class ShowCodeComponent extends BaseComponent implements AfterViewInit, O
 		this.options.component = this.rawContent?.nativeElement?.firstChild.localName;
 	}
 
-	copyToClipboard(): void {
+	/**
+	 * Copies the selected component's code to the clipboard. Adds/removes 'dark:' variants if the user selected the param.
+	 * @param dropdownDarkTheme If the user selected light/dark theme from the dropdown when copying a component. UI theme by default.
+	 */
+	copyToClipboard(dropdownDarkTheme: boolean = this.options.darkTheme): void {
 		this.copied = true;
+		this.dropdownOpen = false;
 		this.formatter.copyToClipboard(this.prettyCode);
-		this.analytics.triggerEvent(PlausibleEvent.COPY_CODE, this.options);
+		const analyticsOptions = Object.assign({}, this.options);
+		analyticsOptions.darkTheme = dropdownDarkTheme;
+		this.analytics.triggerEvent(PlausibleEvent.COPY_CODE, analyticsOptions);
+		setTimeout(() => {
+			this.copied = false;
+		}, 3000);
 	}
 
 	showPreview(): void {
 		this.codeVisible = false;
+		this.dropdownOpen = false;
 		this.copied = false;
 		this.options.language = '';
 	}
@@ -120,8 +132,22 @@ export class ShowCodeComponent extends BaseComponent implements AfterViewInit, O
 		return this.rawCode;
 	}
 
+	copyLightMode() {
+		this.prettyCode = this.formatter.toggleDarkModeVariants(this.prettyCode, false);
+		this.copyToClipboard();
+	}
+
+	copyDarkMode() {
+		this.prettyCode = this.formatter.toggleDarkModeVariants(this.prettyCode, true);
+		this.copyToClipboard();
+	}
+
 	showFullscreen(): void {
 		this.modal.open(this.getRawCode());
+	}
+
+	toggleDropdown() {
+		this.dropdownOpen = !this.dropdownOpen;
 	}
 
 	ngOnDestroy(): void {
