a1bdf1f52cee53cb1178f28575d8c1c4595e29dc
Mirko Nowak <mirko.nowak@outlook.com>
2023-06-12 20:42:34 +0300

    test: settings tests

--- cypress/tests/Settings.cy.ts
+++ cypress/tests/Settings.cy.ts
@@ -1,4 +1,5 @@
 import {SettingsComponent} from '@shared/components/settings/settings.component';
+import {selectors} from 'cypress/support/selectors';
 
 const numberOfTailwindColors = 17;
 
@@ -7,44 +8,38 @@ describe('SettingsComponent', () => {
 		cy.mount(SettingsComponent);
 	});
 
-	it('should be closed by default', () => {
-		cy.get('[data-cy=settings-component]').should('have.class', '-translate-x-full');
-	});
-
-	it('should have violet as the default color', () => {
-		cy.get('[data-cy=settings-selected-color]').contains('violet', {matchCase: false});
-	});
-
-	it('should have dark mode by default', () => {
-		cy.get('[data-cy=settings-dark-mode-toggle]').should('be.checked');
+	it('should be closed, have darkmode on and violet color by default', () => {
+		cy.get(selectors.settings.component).should('have.class', '-translate-x-full');
+		cy.get(selectors.settings.darkModeToggle).should('be.checked');
+		cy.get(selectors.settings.selectedColorName).contains('violet', {matchCase: false});
 	});
 
 	it('should have all the Tailwind CSS colors available', () => {
-		cy.get('[data-cy=settings-colors-grid]')
+		cy.get(selectors.settings.colorGrid)
 			.find('button')
 			.should('have.length', numberOfTailwindColors);
 	});
 
 	it('should be able to be opened', () => {
-		cy.get('[data-cy=settings-toggle]').click();
-		cy.get('[data-cy=settings-component]').should('have.class', 'translate-x-0');
+		cy.get(selectors.settings.toggleOpenButton).click();
+		cy.get(selectors.settings.component).should('have.class', 'translate-x-0');
 	});
 
 	it('should be able to select other colors', () => {
-		cy.get('[data-cy=settings-toggle]').click();
-		cy.get('[data-cy=settings-colors-grid]').find('button').first().click();
-		cy.get('[data-cy=settings-selected-color]').contains('red', {matchCase: false});
+		cy.get(selectors.settings.toggleOpenButton).click();
+		cy.get(selectors.settings.colorGrid).find('button').first().click();
+		cy.get(selectors.settings.selectedColorName).contains('red', {matchCase: false});
 	});
 
 	it('should be able to toggle darkmode', () => {
-		cy.get('[data-cy=settings-toggle]').click();
-		cy.get('[data-cy=settings-dark-mode-toggle]').click();
-		cy.get('[data-cy=settings-dark-mode-toggle]').should('not.be.checked');
+		cy.get(selectors.settings.toggleOpenButton).click();
+		cy.get(selectors.settings.darkModeToggle).click();
+		cy.get(selectors.settings.darkModeToggle).should('not.be.checked');
 	});
 
 	it('should be able to be closed', () => {
-		cy.get('[data-cy=settings-toggle]').click();
-		cy.get('[data-cy=settings-toggle]').click();
-		cy.get('[data-cy=settings-component]').should('have.class', '-translate-x-full');
+		cy.get(selectors.settings.toggleOpenButton).click();
+		cy.get(selectors.settings.toggleOpenButton).click();
+		cy.get(selectors.settings.component).should('have.class', '-translate-x-full');
 	});
 });
