51603e1ed9c0feca1ee19534b53e4c059f8eddbe
Mirko Nowak <mirko.nowak@outlook.com>
2023-07-26 14:50:38 +0300

    feat: showcase ad

--- src/app/shared/components/showcase/showcase.component.ts
+++ src/app/shared/components/showcase/showcase.component.ts
@@ -1,14 +1,19 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, Input} from '@angular/core';
 import {BaseComponent} from '@shared/components/base/base.component';
+import {ethicalads} from 'src/assets/js/ads';
 
 @Component({
 	selector: 'custom-showcase',
 	templateUrl: './showcase.component.html',
 })
-export class ShowcaseComponent extends BaseComponent {
+export class ShowcaseComponent extends BaseComponent implements AfterViewInit {
 	@Input() title = '';
 
 	constructor() {
 		super();
+	}
+
+	ngAfterViewInit(): void {
+		ethicalads.reload();
 	}
 }
