62c429089b8d9e27fd50b1ebe7123c60951f0dcd
Mirko Nowak <mirko.nowak@outlook.com>
2023-07-28 22:53:51 +0300

    fix: sticky ad

--- src/app/shared/components/showcase/showcase.component.ts
+++ src/app/shared/components/showcase/showcase.component.ts
@@ -1,19 +1,14 @@
-import {AfterViewInit, Component, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {BaseComponent} from '@shared/components/base/base.component';
-import {ethicalads} from 'src/assets/js/ads';
 
 @Component({
 	selector: 'custom-showcase',
 	templateUrl: './showcase.component.html',
 })
-export class ShowcaseComponent extends BaseComponent implements AfterViewInit {
+export class ShowcaseComponent extends BaseComponent {
 	@Input() title = '';
 
 	constructor() {
 		super();
-	}
-
-	ngAfterViewInit(): void {
-		ethicalads.reload();
 	}
 }
