ccb64ffcf496a464b9096366c0abcee7b34e2c8a
Mirko <microvvavve@gmail.com>
2023-07-28 13:09:53 +0300

    feat: refresh only when in view

--- src/app/shared/components/ads/ads.component.ts
+++ src/app/shared/components/ads/ads.component.ts
@@ -1,15 +1,25 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {
+	AfterViewInit,
+	Component,
+	ElementRef,
+	Input,
+	OnDestroy,
+	OnInit,
+	ViewChild,
+} from '@angular/core';
 import {ethicalads} from 'src/assets/js/ads';
 
 @Component({
 	selector: 'ads',
 	templateUrl: './ads.component.html',
 })
-export class AdsComponent implements OnInit, OnDestroy {
+export class AdsComponent implements OnInit, OnDestroy, AfterViewInit {
+	@ViewChild('ad') ad: ElementRef | undefined;
 	@Input() sticky: boolean = false;
 	@Input() fixed: boolean = false;
 	@Input() keyword = '';
-	keywords = 'tailwind|tailwindcss|css|component|template|web development';
+	keywords =
+		'angular|reactjs|vuejs|frontend|javascript|typescript|css|tailwindcss|aws|database|security|redis|testing|ai';
 	interval;
 
 	constructor() {}
@@ -19,12 +29,28 @@ export class AdsComponent implements OnInit, OnDestroy {
 			this.keywords += '|' + this.keyword;
 		}
 		ethicalads.reload();
-		this.interval = setInterval(() => {
-			ethicalads.reload();
-		}, 17000);
+	}
+
+	ngAfterViewInit(): void {
+		this.refreshAdsInIntervalIfElementVisible();
 	}
 
 	ngOnDestroy(): void {
 		clearInterval(this.interval);
+	}
+
+	refreshAdsInIntervalIfElementVisible() {
+		const observer = new IntersectionObserver((entries, obs) => {
+			entries.forEach(entry => {
+				clearInterval(this.interval);
+				if (entry.isIntersecting) {
+					this.interval = setInterval(() => {
+						ethicalads.reload();
+					}, 17000);
+				}
+			});
+		});
+
+		observer.observe(this.ad?.nativeElement);
 	}
 }
