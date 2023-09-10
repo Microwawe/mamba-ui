22609259e411221b3e239f973c2ee78758215bd8
Mirko <microvvavve@gmail.com>
2023-09-10 12:53:42 +0300

    chore: update footer component count

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	roundedComponentTotal: 100,
+	roundedComponentTotal: 150,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
 	production: false,
-	roundedComponentTotal: 100,
+	roundedComponentTotal: 150,
 };
 
 /*
