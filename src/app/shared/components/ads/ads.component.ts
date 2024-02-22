2c59ab0fbaa6db19f22ce07181105f8257e27b56
Mirko Nowak <mirko.nowak@outlook.com>
2024-02-22 22:07:07 +0200

    fix: ads in ssr

--- src/app/shared/components/ads/ads.component.ts
+++ src/app/shared/components/ads/ads.component.ts
@@ -2,25 +2,31 @@ import {
 	AfterViewInit,
 	Component,
 	ElementRef,
+	Inject,
 	Input,
 	OnDestroy,
 	OnInit,
+	PLATFORM_ID,
 	ViewChild,
-	afterNextRender,
 } from '@angular/core';
-// import {ethicalads} from 'src/assets/js/ads';
+import ethicalads from 'src/assets/js/ads';
 import {BaseComponent} from '../base/base.component';
 import {NavigationEnd, Router} from '@angular/router';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
 	selector: 'ads',
 	templateUrl: './ads.component.html',
 })
-export class AdsComponent extends BaseComponent implements OnInit, OnDestroy {
-	@ViewChild('ad') ad: ElementRef | undefined;
+export class AdsComponent extends BaseComponent implements AfterViewInit, OnDestroy {
 	@Input() sticky: boolean = false;
 	@Input() fixed: boolean = false;
+	@Input() showcase: boolean = false;
 	@Input() keyword = '';
+	@ViewChild('fixedAd') fixedAd: ElementRef | undefined;
+	@ViewChild('stickyAd') stickyAd: ElementRef | undefined;
+	@ViewChild('showcaseAd') showcaseAd: ElementRef | undefined;
+
 	keywords =
 		'pdf|document|aws|gcp|azure|database|search|ai|scalable|serverless|cloud|devops|docker|kubernetes|microservices|mysql|postgresql|postgres|oauth|ml|react|angular|vue';
 	interval;
@@ -30,34 +36,36 @@ export class AdsComponent extends BaseComponent implements OnInit, OnDestroy {
 	minViewTimeMs = 2000;
 	maxViewTimeMs = 8000;
 
-	constructor(private router: Router) {
+	constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {
 		super();
-		afterNextRender(() => {
-			// ethicalads.reload();
-			this.refreshAdsInIntervalIfElementVisible();
-		});
 	}
 
-	ngOnInit() {
+	ngAfterViewInit() {
 		if (this.keyword) {
 			this.keywords += '|' + this.keyword;
 		}
 
-		this.routerSubscription = this.router.events.subscribe(event => {
-			if (event instanceof NavigationEnd) {
-				const currentTime = Date.now();
-				if (currentTime - this.lastNavigationTime > this.minViewTimeMs) {
-					// ethicalads.reload();
-					this.manuallyClosed = false;
-					this.lastNavigationTime = currentTime;
+		if (isPlatformBrowser(this.platformId)) {
+			ethicalads.reload();
+			this.routerSubscription = this.router.events.subscribe(event => {
+				if (event instanceof NavigationEnd) {
+					const currentTime = Date.now();
+					if (currentTime - this.lastNavigationTime > this.minViewTimeMs) {
+						ethicalads.reload();
+						this.manuallyClosed = false;
+						this.lastNavigationTime = currentTime;
+					}
 				}
-			}
-		});
+			});
+			this.refreshAdsInIntervalIfElementVisible();
+		}
 	}
 
 	ngOnDestroy(): void {
 		clearInterval(this.interval);
-		this.routerSubscription.unsubscribe();
+		if (this.routerSubscription) {
+			this.routerSubscription?.unsubscribe();
+		}
 	}
 
 	refreshAdsInIntervalIfElementVisible() {
@@ -66,14 +74,20 @@ export class AdsComponent extends BaseComponent implements OnInit, OnDestroy {
 				clearInterval(this.interval);
 				this.interval = setInterval(() => {
 					if (entry.isIntersecting) {
-						// ethicalads.reload();
+						ethicalads.reload();
 					}
 				}, this.maxViewTimeMs);
 			});
 		});
 
-		if (this.ad) {
-			observer.observe(this.ad.nativeElement);
+		if (this.fixedAd) {
+			observer.observe(this.fixedAd.nativeElement);
+		}
+		if (this.stickyAd) {
+			observer.observe(this.stickyAd.nativeElement);
+		}
+		if (this.showcaseAd) {
+			observer.observe(this.showcaseAd.nativeElement);
 		}
 	}
 
